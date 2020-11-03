fe817df9e5dfe955a0e4e68567c1b00fbcc457e3
rzunigat <rodrigo-uriel.zuniga-tellez@etu.utc.fr>
2020-11-03 20:20:49 +0100

    Fix: Diploma/Project to Upper Case

--- src/components/PxExperienceContent.vue
+++ src/components/PxExperienceContent.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <px-title :title="diploma" font-style="font-serif" size="text-md" color="text-black" class="block" />
+        <px-title :title="diploma.toUpperCase()" font-style="font-serif" size="text-md" color="text-black" class="block" />
         <p class="text-gray-600 pb-2" >{{description}}</p>
         <div class="inline-flex">
             <img src="@/assets/location_logo.png" class="w-6 h-6 mr-2"><p class="text-blue-600" >{{location}}</p>
